bc35f7dcd7937978bb6d3e2e228118db2216014d
Joshua Flores <joshuaflores@joshuas-mbp.myfiosgateway.com>
2021-09-30 10:33:50 -0400

    deleted unnecessary files

--- stringFunctions.test.js
+++
@@ -1,9 +0,0 @@
-import { capitalize, reverseString } from './stringFunctions';
-
-test('Capitalizes first letter of a string', () => {
-  expect(capitalize('string')).toMatch(/String/);
-});
-
-test('Reverses string', () => {
-  expect(reverseString('string')).toMatch('gnirts');
-});
